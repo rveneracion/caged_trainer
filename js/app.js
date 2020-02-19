0b41ee6047f12eee216fe0e1530557be65199645
Richard Veneracion <richard.veneracion@gmail.com>
2020-02-18 22:46:42 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    newCombo();
+    $("#b_tryagain").click(function(){
+        newCombo();
+
+    });
+    
+
+});
+
+var newCombo = function(){
+    newKey();
+    newShape();
+}
+
+var randomChoice = function(mylist){
+    var choice = Math.floor(Math.random() * mylist.length)
+    return mylist[choice]
+}
+
+var newKey = function(){
+    let keys = 'ABCDEF'.split('');
+    let sharpflat = ['<sup class="accidental">#</sup>','<sup class="accidental">b</sup>','',''];
+    $("#h_key").html(randomChoice(keys) + randomChoice(sharpflat));
+    // $("#h_key").text('woohoo');
+}
+
+var newShape = function(){
+    let shapes = 'CAGED'.split('');
+    $("#h_shape").html(randomChoice(shapes));
+}
