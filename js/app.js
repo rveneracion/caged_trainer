7107defe3ca630d23c6e2fe6a580ff065d2bd65b
Richard Veneracion <richard.veneracion@gmail.com>
2021-05-26 19:38:08 -0400

    modified keys so there are only the 12 keys of the circle of fifths

--- js/app.js
+++ js/app.js
@@ -21,10 +21,11 @@ var randomChoice = function(mylist){
 }
 
 var newKey = function(){
-    let keys = 'ABCDEF'.split('');
-    let sharpflat = ['<sup class="accidental">&#x266f</sup>','<sup class="accidental">&#x266d</sup>','',''];
-    $("#h_key").html(randomChoice(keys) + randomChoice(sharpflat));
-    // $("#h_key").text('woohoo');
+    let sharp = '<sup class="accidental">&#x266f</sup>';
+    let flat = '<sup class="accidental">&#x266d</sup>';
+    let keys = 'C:G:D:A:E:B:F+:D-:A-:E-:B-:F'.replace(/-/g, flat).replace(/\+/g, sharp).split(':');
+    // let sharpflat = ['<sup class="accidental">&#x266f</sup>','<sup class="accidental">&#x266d</sup>','',''];
+    $("#h_key").html( randomChoice(keys) );
 }
 
 var newShape = function(){
